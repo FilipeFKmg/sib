74c16a1fe98ec1eaed6c09ce7458cd1b79608cac
FilipeFKmg <filipekuhlmann13@gmail.com>
2026-09-23 15:42:08 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
+__author__ = "Filipe Kuhlmann"
+__credits__ = ["Filipe Kuhlmann"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "Filipe Kuhlmann"
+__email__ = "pg42492@uminho.pt"
 
 
 
